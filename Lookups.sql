bc47cf1395933fd0ecc293cf3d31766ace88b574
Ilija Vukotic <ivukotic@uchicago.edu>
2023-10-24 14:35:16 -0500

    fixes

--- Lookups.sql
+++
@@ -1,5 +0,0 @@
--- SELECT DISTINCT name FROM LogVols;
--- SELECT * from LogVols WHERE name = 'Pixel';
-SELECT *
-from ChildrenPositions
-where parentId = 1;
--- geo.session.sql
+++ geo.session.sql
@@ -1,4 +1,7 @@
 -- SELECT * from FullPhysVols where id=4089;
 -- SELECT * from LogVols where id=10001;
-SELECT DISTINCT parentTable
-from ChildrenPositions;
+-- SELECT DISTINCT parentTable
+-- from ChildrenPositions;
+SELECT *
+from ChildrenPositions
+where parentTable = 10;
